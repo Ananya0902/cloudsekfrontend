9367c8f21af5e822e42c1b3510c301ed919de172
Ananya <ananyasrivastava922@gmail.com>
2024-11-11 00:16:21 +0530

    protected roting done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,61 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import ForgotPassword from './Pages/ForgotPassword';
 import Otp from './Pages/Otp';
-
-
 import PostCreation from './Pages/PostCreation';
-
-import './App.css';
-import PostPage from './PostPage';
+import PostPage from './Pages/PostPage';
 import NewPasswordPage from './Pages/NewPasswordPage';
 import MyPosts from './Pages/MyPosts';
+import { AuthProvider } from './AuthContext';  // Import AuthProvider
+import ProtectedRoute from './ProtectedRoute';  // Import ProtectedRoute
 
+import './App.css';
 
 const App: React.FC = () => {
   const [username, setUsername] = useState('User123'); // Example username state
+
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/otp-page" element={<Otp />} />
-          <Route path="/post-creation" element={<PostCreation username={username} />} />
+    <AuthProvider>
+      <Router>
+        <div className="app-container">
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/otp-page" element={<Otp />} />
+
+            {/* <Route path="/post-creation" element={<PostCreation username={username} />} />
           <Route path="/post-page" element={<PostPage />} />
           <Route path="/newpassword" element={<NewPasswordPage />} />
-          <Route path="/myposts" element={<MyPosts />} />
-          
-        </Routes>
-      </div>
-    </Router>
+          <Route path="/myposts" element={<MyPosts />} /> */}
+
+            {/* Protected Routes */}
+            
+            <Route
+              path="/post-creation"
+              element={<ProtectedRoute element={<PostCreation username={username} />} path="/post-creation" />}
+            />
+            <Route
+              path="/post-page"
+              element={<ProtectedRoute element={<PostPage />} path="/post-page" />}
+            />
+            <Route
+              path="/newpassword"
+              element={<ProtectedRoute element={<NewPasswordPage />} path="/newpassword" />}
+            />
+            <Route
+              path="/myposts"
+              element={<ProtectedRoute element={<MyPosts />} path="/myposts" />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 
